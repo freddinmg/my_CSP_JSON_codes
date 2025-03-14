2daf746d8cec32f4ca4bb4d12c8b33b94300698a
freddinmg <79553156+freddinmg@users.noreply.github.com>
2025-03-14 08:32:47 -0500

    Create getImages.js

---
+++ getImages.js
@@ -0,0 +1,21 @@
+function doGet() {
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Gradients_Links");
+    var data = sheet.getDataRange().getValues();
+    var jsonData = {};
+
+    // Assume first row is headers
+    var headers = data[1];
+    jsonData['data'] = [];
+
+    // Start from row 1 to skip headers
+    for (var i = 2; i < data.length; i++) {
+        var row = {};
+        for (var j = 0; j < headers.length; j++) {
+            row[headers[j]] = data[i][j];
+        }
+        jsonData['data'].push(row);
+    }
+
+    return ContentService.createTextOutput(JSON.stringify(jsonData))
+        .setMimeType(ContentService.MimeType.JSON);
+}
